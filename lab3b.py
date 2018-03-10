aac516501c4665d98d20525827ece6bc5648664d
katiecai <katiecai@g.ucla.edu>
2018-03-10 12:28:08 -0800

    added option parse

--- lab3b.py
+++ lab3b.py
@@ -1,1 +1,11 @@
-    
+import sys
+import csv
+
+def main():
+    if sys.argc != 2:
+        print("Wrong number of arguments")
+        sys.exit()
+    with open(sys.argv[1], 'r') as my_file:
+
+if __name__ == "__main__":
+    main()
